c1228e84cbea53ca5ec30d66f89598daa4581309
Daniel Cairol <dcairol@gmail.com>
2014-09-24 22:31:28 -0600

    Add simple/basic navigation to item details

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -2,12 +2,17 @@ define(['angular'],function ( angular ){
 
 	return angular.module('myApp.controllers',[])
 	.controller('CartController', function CartController( $scope ){
-	  $scope.items = [{name: 'Leche', price: 1000, quantity: 2}];
+	  var id = 0;
+	  $scope.items = [{name: 'Leche', price: 1000, quantity: 2, id: id++}];
 
 	  $scope.newItem = function newItem(){
 	    $scope.items.push({name: this.name, price: this.price, quantity: this.quantity });
 	    this.name = '';
 	    this.price = '';
-	  }
+	    this.id = id++;
+	  };
+	})
+	.controller('ItemsController',function ItemsController($scope,$routeParams){
+		$scope.name = $routeParams.name;
 	});
 });
--- scripts/routes.js
+++ scripts/routes.js
@@ -1,12 +1,16 @@
 define(['angular', 'app'],function(angular, my_app){
-	return my_app.config(['$routeProvider',function($routeProvider){
+	return my_app.config(function($routeProvider){
 		$routeProvider.
 		  when('/',{
 		  	templateUrl: 'views/cart.html',
 		  	controller: 'CartController'
 		  }).
+		  when('/items/:name',{
+		  	templateUrl: 'views/item.html',
+		  	controller: 'ItemsController'
+		  }).
 		  otherwise({
 		  	redirecTo: '/'
 		  });
-	}]);
+	});
 });
